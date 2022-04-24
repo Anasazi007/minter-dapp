2abbdf39a2162162358393afd153170ad8126fad
Anasazi007 <marc.r.wilson.1.9.6.9@gmail.com>
2022-04-23 22:57:48 -0400

    updated index.html & constants.js files

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0xa5A5C3dE70eB21679825dF4Cdb8014c6D8dC494E"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the Beta Collection!";
+const welcome_h1 = "Welcome to Dice Collection 6969A!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Beta NFT Collection is a decentralized project created for test purposes.';
+const welcome_p = 'Dice Collection 6969A is a decentralized project created for test purposes.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
